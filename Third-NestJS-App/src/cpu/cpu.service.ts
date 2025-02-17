1396d7e7a715e0c6beeabcdcdfb5d6d8dc8014da
Dinu98 <alexdinescu888@gmail.com>
2025-02-17 18:14:01 +0200

    More of sharing services between modules

--- Third-NestJS-App/src/cpu/cpu.service.ts
+++ Third-NestJS-App/src/cpu/cpu.service.ts
@@ -4,4 +4,11 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(number1: number, number2: number) {
+    console.log(`Making some computations`);
+    this.powerService.supplyPower(5);
+
+    return number1 + number2;
+  }
 }
